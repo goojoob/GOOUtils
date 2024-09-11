82d2f501ce98352e60b07ce9632e07f458b25080
Antonio Rodríguez <antrorc@gmail.com>
2024-09-11 19:57:56 +0200

    Update ExtensionUIScreen.swift

--- Sources/GOOUtils/Extensions/ExtensionUIScreen.swift
+++ Sources/GOOUtils/Extensions/ExtensionUIScreen.swift
@@ -1,6 +1,24 @@
-import SwiftUI
+#if os(macOS)
 
-#if !os(macOS)
+import AppKit
+
+extension NSScreen {
+    static public var screenWidth: CGFloat {
+        return NSScreen.main?.frame.size.width ?? 0
+    }
+
+    static public var screenHeight: CGFloat {
+        return NSScreen.main?.frame.size.height ?? 0
+    }
+
+    static public var screenSize: CGSize {
+        return NSScreen.main?.frame.size ?? .zero
+    }
+}
+
+#else
+
+import SwiftUI
 
 extension UIScreen {
     static public let screenWidth = UIScreen.main.bounds.size.width
